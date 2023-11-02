a207af4ad5e98229c451859fccea019bdc9c74d4
Suraj11000 <surajshindecs@gmail.com>
2023-11-02 14:31:59 +0530

    static Cart page done

--- src/RouterPages/Cart.jsx
+++ src/RouterPages/Cart.jsx
@@ -1,12 +1,66 @@
 import { Fragment } from "react";
+import { useNavigate } from "react-router";
 import Quality from "../Components/Quality";
-import HeroImg from "../assets/images/back-img.jpg";
 import Hero from "../Components/Hero";
+import gridImg from "../assets/images/grid.jpg";
 
 const Cart = () => {
+  const Navigate = useNavigate();
   return (
     <Fragment>
       <Hero title="Cart" />
+      <section className="w-full flex justify-center px-[10%] py-[4%]">
+        <div className="w-full gap-5 flex ">
+          <div className="w-9/12">
+            <table className="w-full border-collapse">
+              <thead className="flex flex-wrap">
+                <tr className=" bg-[#faf3ea] flex gap-20">
+                  <th></th>
+                  <th></th>
+                  <th className="text-center p-4">Product</th>
+                  <th className="text-center p-4">Price</th>
+                  <th className="text-center p-4">Quantity</th>
+                  <th className="text-center p-4">Subtotal</th>
+                  <th className="text-center p-4"> </th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody className="flex  justify-centre items-center mt-10">
+                <tr className="flex  gap-28 items-center justify-centre">
+                  <td className="text-center p-2">
+                    {" "}
+                    <div className="flex justify-center items-center gap-10">
+                      <img
+                        src={gridImg}
+                        alt=""
+                        className="w-[6rem] rounded-lg"
+                      />
+                      <p>Wall Frame</p>
+                    </div>
+                  </td>
+                  <td className="text-center p-2">Rs. 100</td>
+                  <td className="text-center p-2">1</td>
+                  <td className="text-center p-2"><div className="flex justify-between items-center gap-14"><p>Rs. 1000</p><i className="fa-solid fa-trash text-orange-600"></i></div></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="bg-[#f9f1e7] flex flex-col justify-center items-center p-8 w-80">
+            <h1 className="text-2xl font-bold">Cart Total</h1>
+            <div className=" w-full flex justify-between p-3 px-10 items-center mt-6">
+              <p className="font-semibold">Subtotal</p>
+              <p className="font-semibold">Rs. 1000</p>
+            </div>
+            <div className=" w-full flex justify-between p-3 px-10 items-center mt-6">
+              <p className="font-semibold">Total</p>
+              <p className="font-semibold">Rs. 1000</p>
+            </div>
+            <button class="mt-6 px-8 py-2 rounded-xl border-solid bg-black text-white font-semibold border-[black] hover:bg-[#f9f1e7] hover:text-black border-2" onClick={() => Navigate("/Checkout")}>
+              Check Out
+            </button>
+          </div>
+        </div>
+      </section>
       <Quality />
     </Fragment>
   );
--- src/RouterPages/Home.jsx
+++ src/RouterPages/Home.jsx
@@ -1,7 +1,6 @@
 import dinnigImg from "../assets/images/dinning-1.jpg";
 import livingImg from "../assets/images/living room.jpg";
 import bedroomImg from "../assets/images/bedroom.webp";
-import gridImg from "../assets/images/grid.jpg";
 import HeroImg from "../assets/images/back-img.jpg";
 import React from "react";
 import { useNavigate } from "react-router";
@@ -100,135 +99,7 @@ export default function Home() {
           </button>
         </div>
       </section>
-
       <ProductGrid/>
-      <section className="mt-20">
-        <div className="h-[7.5vh] w-[90vw] bg-[#f4f4f4] mt-4 m-auto rounded-[10px]">
-          <h1 className="justify-center flex items-center text-[2.5rem]">
-            Our Products
-          </h1>
-        </div>
-        <div className="max-w-full h-auto grid place-items-center p-6 md:p-20">
-        <div className="grid gap-y-14 md:grid-cols-[repeat(2,1fr)] md:gap-x-6 xl:grid-cols-[repeat(3,1fr)]">
-          <div className="relative bg-[#f4f5f7] overflow-hidden max-w-[350px] shadow-[3px_3px_10px_2px_rgb(232,232,235)] hover:transition-[0.20s] hover:shadow-[3px_3px_15px_2px_rgb(188,188,189)] hover:scale-105 ">
-            <img
-              src={gridImg}
-              alt="Avatar"
-              className="w-[100%]"
-              onClick={() => Navigate("/SepProduct")}
-            />
-            <div className="flex flex-col gap-[5px] w-full justify-start p-[3%]">
-              <h3 className="font-semibold text-2xl text-[#3a3a3a]">
-                wall frame
-              </h3>
-              <p>Attrative Wall Frames</p>
-              <p className="text-[#898989] text-base flex gap-2.5">
-                <span className="text-[#3a3a3a] font-medium">Rs.1000</span>
-                <del>Rs. 1500.00</del>
-              </p>
-            </div>
-          </div>
-
-          <div className="relative bg-[#f4f5f7] overflow-hidden max-w-[350px] shadow-[3px_3px_10px_2px_rgb(232,232,235)] hover:transition-[0.20s] hover:shadow-[3px_3px_15px_2px_rgb(188,188,189)] hover:scale-105 ">
-            <img
-              src={gridImg}
-              alt="Avatar"
-              className="w-[100%]"
-              onClick={() => Navigate("/SepProduct")}
-            />
-            <div className="flex flex-col gap-[5px] w-full justify-start p-[3%]">
-              <h3 className="font-semibold text-2xl text-[#3a3a3a]">
-                wall frame
-              </h3>
-              <p>Attrative Wall Frames</p>
-              <p className="text-[#898989] text-base flex gap-2.5">
-                <span className="text-[#3a3a3a] font-medium">Rs.1000</span>
-                <del>Rs. 1500.00</del>
-              </p>
-            </div>
-          </div>
-
-          <div className="relative bg-[#f4f5f7] overflow-hidden max-w-[350px] shadow-[3px_3px_10px_2px_rgb(232,232,235)] hover:transition-[0.20s] hover:shadow-[3px_3px_15px_2px_rgb(188,188,189)] hover:scale-105 ">
-            <img
-              src={gridImg}
-              alt="Avatar"
-              className="w-[100%]"
-              onClick={() => Navigate("/SepProduct")}
-            />
-            <div className="flex flex-col gap-[5px] w-full justify-start p-[3%]">
-              <h3 className="font-semibold text-2xl text-[#3a3a3a]">
-                wall frame
-              </h3>
-              <p>Attrative Wall Frames</p>
-              <p className="text-[#898989] text-base flex gap-2.5">
-                <span className="text-[#3a3a3a] font-medium">Rs.1000</span>
-                <del>Rs. 1500.00</del>
-              </p>
-            </div>
-          </div>
-          <div className="relative bg-[#f4f5f7] overflow-hidden max-w-[350px] shadow-[3px_3px_10px_2px_rgb(232,232,235)] hover:transition-[0.20s] hover:shadow-[3px_3px_15px_2px_rgb(188,188,189)] hover:scale-105 ">
-            <img
-              src={gridImg}
-              alt="Avatar"
-              className="w-[100%]"
-              onClick={() => Navigate("/SepProduct")}
-            />
-            <div className="flex flex-col gap-[5px] w-full justify-start p-[3%]">
-              <h3 className="font-semibold text-2xl text-[#3a3a3a]">
-                wall frame
-              </h3>
-              <p>Attrative Wall Frames</p>
-              <p className="text-[#898989] text-base flex gap-2.5">
-                <span className="text-[#3a3a3a] font-medium">Rs.1000</span>
-                <del>Rs. 1500.00</del>
-              </p>
-            </div>
-          </div>
-          <div className="relative bg-[#f4f5f7] overflow-hidden max-w-[350px] shadow-[3px_3px_10px_2px_rgb(232,232,235)] hover:transition-[0.20s] hover:shadow-[3px_3px_15px_2px_rgb(188,188,189)] hover:scale-105 ">
-            <img
-              src={gridImg}
-              alt="Avatar"
-              className="w-[100%]"
-              onClick={() => Navigate("/SepProduct")}
-            />
-            <div className="flex flex-col gap-[5px] w-full justify-start p-[3%]">
-              <h3 className="font-semibold text-2xl text-[#3a3a3a]">
-                wall frame
-              </h3>
-              <p>Attrative Wall Frames</p>
-              <p className="text-[#898989] text-base flex gap-2.5">
-                <span className="text-[#3a3a3a] font-medium">Rs.1000</span>
-                <del>Rs. 1500.00</del>
-              </p>
-            </div>
-          </div>
-          <div className="relative bg-[#f4f5f7] overflow-hidden max-w-[350px] shadow-[3px_3px_10px_2px_rgb(232,232,235)] hover:transition-[0.20s] hover:shadow-[3px_3px_15px_2px_rgb(188,188,189)] hover:scale-105 ">
-            <img
-              src={gridImg}
-              alt="Avatar"
-              className="w-[100%]"
-              onClick={() => Navigate("/SepProduct")}
-            />
-            <div className="flex flex-col gap-[5px] w-full justify-start p-[3%]">
-              <h3 className="font-semibold text-2xl text-[#3a3a3a]">
-                wall frame
-              </h3>
-              <p>Attrative Wall Frames</p>
-              <p className="text-[#898989] text-base flex gap-2.5">
-                <span className="text-[#3a3a3a] font-medium">Rs.1000</span>
-                <del>Rs. 1500.00</del>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-        <button
-          className="flex justify-center m-auto cursor-pointer text-white bg-black text-base mt-8 px-8 py-3 rounded-xl hover:text-[black] hover:bg-[white] hover:border hover:border-solid hover:border-[black] hover:font-semibold"
-          onClick={() => Navigate("/Products")}
-        >
-          Show more
-        </button>
-      </section>
     </main>
   );
 }
