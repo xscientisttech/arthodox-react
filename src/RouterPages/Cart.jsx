a9026b066a131737ae95d2240b2a841590e5a046
subodh <subodhgaikwad70@gmail.com>
2023-11-02 09:45:23 +0530

    _

--- src/RouterPages/Cart.jsx
+++ src/RouterPages/Cart.jsx
@@ -1,27 +1,12 @@
 import { Fragment } from "react";
 import Quality from "../Components/Quality";
 import HeroImg from "../assets/images/back-img.jpg";
+import Hero from "../Components/Hero";
 
 const Cart = () => {
   return (
     <Fragment>
-      <div className="flex justify-center items-center">
-        <div className="w-full h-[25rem]">
-          <img
-            src={HeroImg}
-            alt="back-img"
-            className=" bg-cover bg-no-repeat h-full w-full transform opacity-50"
-          />
-        </div>
-        <div className=" absolute flex flex-col justify-center items-center">
-          <h2 className="text-[3rem] font-bold ">Cart</h2>
-          <p className="">
-            <span className="text-xl font-semibold">Home </span>
-            <i className="fa-solid fa-greater-than"></i>
-            <span className="text-xl font-semibold"> Cart</span>
-          </p>
-        </div>
-      </div>
+      <Hero title="Cart" />
       <Quality />
     </Fragment>
   );
