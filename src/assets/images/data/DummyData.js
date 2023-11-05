a89f346dadc401a227b757e6f7370c890dbed10c
subodh <subodhgaikwad70@gmail.com>
2023-11-05 16:53:04 +0530

    alert added

---
+++ src/assets/images/data/DummyData.js
@@ -0,0 +1,22 @@
+import gridImg from "../grid.jpg";
+import HeroImg from "../back-img.jpg";
+import img1 from "../background/bg1.jpg"
+import img2 from "../background/bg2.jpg"
+import img3 from "../background/bg5.jpg"
+import img4 from "../background/bg6.jpg"
+
+const DummyData = {
+    GridImgUrl : gridImg,
+    HeroImgUrl : HeroImg,
+    CategoryImgUrl: gridImg,
+    BgChangeImgUrl1 : img1,
+    BgChangeImgUrl2 : img2,
+    BgChangeImgUrl3 : img3,
+    BgChangeImgUrl4 : img4,
+    Title: "Wall Frame",
+    Description : "Attractive Wall Frames",
+    Dprice: "Rs.1000",
+    Oprice: "Rs. 1500.00"
+}
+
+export default DummyData;
