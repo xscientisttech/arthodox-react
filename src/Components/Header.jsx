1d99413e465aee9c494333b399c1a00324371d71
Suraj11000 <surajshindecs@gmail.com>
2023-11-07 01:22:36 +0530

    Fixed footer Arthodox logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import HamburgerMenu from "./Hamburger";
 import NavCart from "./navCart";
 import SearchIcon from "./SearchIcon";
 import DummyData from "../assets/data/DummyData";
-import logo from '../assets/images/Arth-Logo.png'
+import logo from "../assets/images/Arth-Logo.png";
 
 const Header = () => {
   const location = useLocation();
@@ -19,44 +19,50 @@ const Header = () => {
   return (
     <div className=" px-5 bg-white border-b shadow-sm sticky top-0 z-50 font-Poppins">
       <header className="flex justify-between items-center xl:px-12 lg:px-12 md:px-12 mx-auto sm:px-6">
-        <div className="flex items-center" >
+        <div className="flex items-center">
           <img
             src={logo}
             alt="Arthodox-logo"
             className="w-10 h-10 cursor-pointer"
             onClick={() => Navigate("/")}
           />
-          <h1 className="text-lg font-bold" >Arthodox</h1>
+          <h1 className="text-lg font-bold cursor-pointer" onClick={() => Navigate("/")}>
+            Arthodox
+          </h1>
         </div>
         <ul className=" flex justify-center gap-10">
           <li
-            className={` cursor-pointer py-3 text-lg text-gray-400 border-b-[3px] border-b-transparent hidden md:block ${pathMatchRoute("/") &&
+            className={` cursor-pointer py-3 text-lg text-gray-400 border-b-[3px] border-b-transparent hidden md:block ${
+              pathMatchRoute("/") &&
               "font-bold text-gray-900 border-b-orange-600"
-              }`}
+            }`}
             onClick={() => Navigate("/")}
           >
             Home
           </li>
           <li
-            className={` cursor-pointer py-3 text-lg text-gray-400 border-b-[3px] border-b-transparent hidden md:block ${pathMatchRoute("/Products") &&
+            className={` cursor-pointer py-3 text-lg text-gray-400 border-b-[3px] border-b-transparent hidden md:block ${
+              pathMatchRoute("/Products") &&
               "border-b-orange-600 font-bold text-gray-900"
-              }`}
+            }`}
             onClick={() => Navigate("/Products")}
           >
             Products
           </li>
           <li
-            className={`cursor-pointer py-3 text-lg text-gray-400 border-b-[3px] border-b-transparent hidden md:block ${pathMatchRoute("/Category") &&
+            className={`cursor-pointer py-3 text-lg text-gray-400 border-b-[3px] border-b-transparent hidden md:block ${
+              pathMatchRoute("/Category") &&
               "border-b-orange-600 font-bold text-gray-900"
-              }`}
+            }`}
             onClick={() => Navigate("/Category")}
           >
             Category
           </li>
           <li
-            className={`cursor-pointer py-3 text-lg text-gray-400 border-b-[3px] border-b-transparent hidden md:block ${pathMatchRoute("/About") &&
+            className={`cursor-pointer py-3 text-lg text-gray-400 border-b-[3px] border-b-transparent hidden md:block ${
+              pathMatchRoute("/About") &&
               "border-b-orange-600 font-bold text-gray-900"
-              }`}
+            }`}
             onClick={() => Navigate("/About")}
           >
             About
