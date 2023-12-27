a312ab130264545e814864d1847629cea71e1029
Suraj11000 <surajshindecs@gmail.com>
2023-12-28 00:59:16 +0530

    changes done

--- src/RouterPages/Category.jsx
+++ src/RouterPages/Category.jsx
@@ -31,7 +31,7 @@ const Category = () => {
         </div>
       </div>
 
-      <PageSection />
+      {/* <PageSection /> */}
       <Quality />
     </Fragment>
   );
--- src/RouterPages/Products.jsx
+++ src/RouterPages/Products.jsx
@@ -31,7 +31,7 @@ const Products = () => {
 
         </div>
       </div>
-      <PageSection />
+      {/* <PageSection /> */}
       <Quality />
     </Fragment>
   );
