13fc287e4d9b9a666b1e925060131531bf14ebb3
Suraj11000 <surajshindecs@gmail.com>
2023-12-30 16:24:05 +0530

    Sort function added

--- src/RouterPages/Products.jsx
+++ src/RouterPages/Products.jsx
@@ -1,72 +1,55 @@
-import { React, useEffect, useState } from "react";
-import Filter from "../Components/Filter";
-import PageSection from "../Components/PageSection";
-import Quality from "../Components/Quality";
-import { Fragment } from "react";
-import Hero from "../Components/Hero";
-import ProductItem from "../Components/ProductItem";
-import DummyData from "../assets/data/DummyData";
+import React, { useEffect, useState, Fragment } from "react";
 import { useProducts } from '../assets/data/ProductContext';
 import { useParams } from "react-router";
-
+import Filter from "../Components/Filter";
+import Hero from "../Components/Hero";
+import ProductItem from "../Components/ProductItem";
 
 const Products = ({ search }) => {
-
   const products = useProducts();
   const { categoryId } = useParams();
-  const [category, setCategory] = useState( categoryId || 0);
+  const [category, setCategory] = useState(categoryId || 0);
+  const [sortBy, setSortBy] = useState("null");
 
   useEffect(() => {
     window.scrollTo(0, 0);
-
     if (categoryId) {
-      setCategory(categoryId)
+      setCategory(categoryId);
+    }
+  }, [categoryId]);
+
+  const filteredProducts = products
+    .filter((item) => {
+      return search.toLowerCase() === '' ? item : item.title.toLowerCase().includes(search);
+    })
+    .filter((item) => category === 0 || item.category === parseInt(category));
+
+  const sortedProducts = [...filteredProducts].sort((a, b) => {
+    switch (sortBy) {
+      case "1":
+        return a.Discount - b.Discount;
+      case "2":
+        return b.Discount - a.Discount;
+      case "3":
+        return a.title.localeCompare(b.title);
+      case "4":
+        return b.title.localeCompare(a.title);
+      default:
+        return 0;
     }
   });
-
-  useEffect(() => {
-    console.log(search);
-  }, [search])
-
 
   return (
     <Fragment>
       <Hero title="Products" />
-      <Filter />
+      <Filter sortBy={sortBy} setSortBy={setSortBy} />
       <div className="max-w-full h-auto grid place-items-center p-6 md:p-20 bg-[#f4f5f7]">
         <div className="grid gap-y-14 md:grid-cols-[repeat(2,1fr)] md:gap-x-6 xl:grid-cols-[repeat(3,1fr)]">
-
-          {category !== 0 ? products.filter((item) => {
-              return search.toLowerCase() === '' 
-              ? item
-              : item.title.toLowerCase().includes(search);
-            })
-            .map((product, index) => (
-              <>
-                {category == product.category && <ProductItem key={index} product={product} />}
-              </>
-            ))
-
-            : products.filter((item) => {
-              return search.toLowerCase() === '' 
-              ? item
-              : item.title.toLowerCase().includes(search);
-            })
-            .map((product, index) => (
-              <>
-                <ProductItem key={index} product={product} />
-              </>
-            ))
-          }
-
-{/* {category == product.category && <ProductItem key={index} product={product} />} */}
-
-
-
+          {sortedProducts.map((product, index) => (
+            <ProductItem key={index} product={product} />
+          ))}
         </div>
       </div>
-      {/* <PageSection /> */}
-      <Quality />
     </Fragment>
   );
 };
