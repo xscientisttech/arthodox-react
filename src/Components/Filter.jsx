152f4bc6a64c1469bcff97e5e2ff1bdd7a8d250a
Suraj11000 <surajshindecs@gmail.com>
2024-02-08 16:01:11 +0530

    added arrow

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { FaSliders } from "react-icons/fa6";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 const Filter = ({ sortBy, setSortBy }) => {
   return (
     <div className="bg-[#F9F1E7] h-[70px] w-full flex justify-around items-center font-Poppins">
       <div className="flex gap-5">
-        <div className="flex gap-2.5 justify-between cursor-pointer items-center">
-
-        </div>
+        <div className="flex gap-2.5 justify-between cursor-pointer items-center"></div>
       </div>
       <div className="flex gap-5">
-        <div className="sort-by">
+        <div className="sort-by flex">
           <label className="relative flex w-40 md:w-60 rounded overflow-hidden">
             <select
               className="appearance-none text-inherit shadow-none flex-1 cursor-pointer p-[0.7em] border-0"
@@ -24,6 +23,13 @@ const Filter = ({ sortBy, setSortBy }) => {
               <option value="4">Descending Z-A</option>
             </select>
           </label>
+          <div
+            className=" relative flex justify-center items-center right-10"
+            value={sortBy}
+            onClick={(e) => setSortBy(e.target.value)}
+          >
+            <IoMdArrowDropdown />
+          </div>
         </div>
       </div>
     </div>
